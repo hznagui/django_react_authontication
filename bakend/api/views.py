3279369fa0766acf6d4b629a7e02e4dc88879890
Houdaifa Znagui <hznagui@e2r9p5.1337.ma>
2025-01-01 11:50:47 +0100

    create a register view and a url for her

--- bakend/api/views.py
+++ bakend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+#heaeder that was adding 
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerialazers
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
