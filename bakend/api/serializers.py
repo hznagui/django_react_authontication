042a20a90b249d483fb86f860b1776e119f5969d
Houdaifa Znagui <hznagui@e2r9p5.1337.ma>
2025-01-01 11:34:15 +0100

    create a module serializers for user

--- bakend/api/serializers.py
+++ bakend/api/serializers.py
@@ -1,3 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
+
+class UserSerialazers(serializers.ModelSerializer):
+    class Meta:
+        Model= User
+        fields=["id","username","password"]
+        extra_kwargs={"password": {"write_only": True}}
+    def create(self, validated_data):
+        user=User.objects.create_user(**validated_data)
+        return user
+
+                
     
