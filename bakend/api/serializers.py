ede3b8ce91c2c010c4e4424128ce727a40646a72
hznagui <houdayfahouayfa7@gmail.com>
2025-01-01 14:07:29 +0100

    now the register is working

--- bakend/api/serializers.py
+++ bakend/api/serializers.py
@@ -8,7 +8,7 @@ class UserSerialazers(serializers.ModelSerializer):
         fields=["id","username","password"]
         extra_kwargs={"password": {"write_only": True}}
     def create(self, validated_data):
-        user=User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
 
                 
