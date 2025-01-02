86c2e1c41301f358c04e7ca674384c0615cc658c
hznagui <houdayfahouayfa7@gmail.com>
2025-01-02 11:39:53 +0100

    start knowing how to query

--- bakend/api/views.py
+++ bakend/api/views.py
@@ -3,10 +3,20 @@ from django.shortcuts import render
 #heaeder that was adding 
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerialazers
+from .serializers import UserSerialazers, NoteSerialazers
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerialazers
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        return Note.objects.filter(author = user) 
+        
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
